27007b76a704a4bacadf6c4a418c9416ec0b2da8
ayinugrahaaa <34712632+ayinugrahaaa@users.noreply.github.com>
2018-05-13 20:32:56 +0700

    index.php

--- index.php
+++ index.php
@@ -14,7 +14,6 @@
 			<center><h4>Sign In</h4>
 			<form method ="POST"  action="login.php">
 			<center>
-			<input type="text"  name="email" class="textbox" placeholder="Email"></input>
 			<input type="text"  name="username" class="textbox" placeholder="Username"></input>
 			<input type="password"  name="password" class="textbox" placeholder="Password"></input
 			<span style="padding-left:2em"><input type="submit" name="login" size="5px" value="Login" id="button"></span>
